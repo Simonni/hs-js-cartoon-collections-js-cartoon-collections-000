b219164f04e4a547c5c5d7c5ea9d7cefd3532a67
Simon Kiflay <szkiflay@gmail.com>
2018-09-04 21:04:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+  var dwarf = '';
+for (let i = 0; i<dwarves.length; i++){
+    dwarf += (`${i+1}. ${dwarves[i]} `);
+  }
+return dwarf;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+   var caps = [];
+  for(let i =0; i<planeteerCalls.length; i++){
+    caps.push(`${planeteerCalls[i].toUpperCase()}!`);
+  
+  }
+  return caps;
 }
 
+
 function longPlaneteerCalls(words) {
+  for (let i=0; i<words.length; i++){
+    if(words[i].length > 4){
+      return true;
+    }
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
+  for (let i=0; i<foods.length; i++){
+    if (foods[i]==="cheddar" || foods[i]==="gouda" || foods[i]==="camembert" ){
+      return foods[i];
+    }
+  }
+  return "no cheese!";
 }
